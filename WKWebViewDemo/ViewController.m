e6514745b9cd9e769967976b45205d175d586ec6
NiceForMe <771717844@qq.com>
2022-12-09 18:42:54 +0800

    fix

--- WKWebViewDemo/ViewController.m
+++ WKWebViewDemo/ViewController.m
@@ -6,8 +6,9 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+@interface ViewController () <WKUIDelegate, WKNavigationDelegate>
 
 @end
 
@@ -15,7 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    WKWebViewConfiguration *wkWebConfig = [[WKWebViewConfiguration alloc] init];
+    wkWebConfig.websiteDataStore = [WKWebsiteDataStore defaultDataStore];
+    
+    WKWebView *view = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:wkWebConfig];
+    //遵循这个delegate 点“去这里”页面展示有问题，不遵循就没问题
+    view.navigationDelegate = self;
+    view.UIDelegate = self;
+    
+    NSString *url = @"https://uri.amap.com/marker?position=116.478346,39.997361";
+    
+    [view loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
+    [self.view addSubview:view];
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    NSLog(@"Debug --- decidePolicyForNavigationAction url %@", webView.URL.description);
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+//这里会报 Error Domain=NSURLErrorDomain Code=-1002 "unsupported URL"
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error
+{
+    NSLog(@"Debug --- didFailProvisionalNavigation url %@", webView.URL.description);
 }
 
 
